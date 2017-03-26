5c122ab7c56ae3c01ed9e8a5d8853e9356f84d3a
mshahin364 <mshahin364@gmail.com>
2017-03-26 13:06:08 +0600

    proble_type data presentation change according to mock

--- src/app/modules/admin/problem_type/problem_type.component.ts
+++ src/app/modules/admin/problem_type/problem_type.component.ts
@@ -8,80 +8,93 @@ import { AuthenticationService } from "app/modules/authentication";
 declare var $: any;
 
 @Component({
-  selector: 'ewo-problem-type-list',
-  templateUrl: 'problem_type.component.html',
+    selector: 'ewo-problem-type-list',
+    templateUrl: 'problem_type.component.html',
 })
 
 export class ProblemTypeComponent implements OnInit {
 
-  problemTypes: any[] = [];
-  currentCompanyId = 1;
+    // problemTypes: any[] = [];
+    problemTypesList1: any[] = [];
+    problemTypesList2: any[] = [];
+    currentCompanyId = 1;
 
-  problemTypeForm = new FormGroup({
-    id: new FormControl(),
-    company: new FormControl(config.api.base + 'company/' + this.currentCompanyId + '/'),
-    problem_name: new FormControl('', Validators.required),
-    tenant_view: new FormControl(false),
-    active: new FormControl('true'),
-    url: new FormControl()
-  });
-
-  problemTypeSearchControl = new FormControl('');
-
-  constructor(
-    private problemTypeService: ProblemTypeService,
-    private authService: AuthenticationService) {
-    this.authService.verifyToken().take(1).subscribe(data => {
-      this.getAllProblemTypes(this.currentCompanyId);
+    problemTypeForm = new FormGroup({
+        id: new FormControl(),
+        company: new FormControl(config.api.base + 'company/' + this.currentCompanyId + '/'),
+        problem_name: new FormControl('', Validators.required),
+        tenant_view: new FormControl(false),
+        active: new FormControl('true'),
+        url: new FormControl()
     });
-  }
 
-  ngOnInit() {
-    $('#modal-add-problem-type').on('hidden.bs.modal', () => {
-      this.closeModal();
-    });
-  }
+    problemTypeSearchControl = new FormControl('');
 
-  getAllProblemTypes(company_id): void {
-    this.problemTypeService.getAllProblemTypes(company_id).subscribe(
-      data => {
-        this.problemTypes = data.results;
-      }
-    );
-  }
-
-  editProblemType(problemType) {
-    this.problemTypeForm.setValue(problemType);
-  }
-
-  onSubmit() {
-console.log(this.problemTypeForm.value);
-    if (this.problemTypeForm.value.id) {
-      this.problemTypeService.update(this.problemTypeForm.value).subscribe((problemType: any) => {
-        this.getAllProblemTypes(this.currentCompanyId);
-        this.closeModal();
-      });
-      return;
+    constructor(
+        private problemTypeService: ProblemTypeService,
+        private authService: AuthenticationService) {
+        this.authService.verifyToken().take(1).subscribe(data => {
+            this.getAllProblemTypes(this.currentCompanyId);
+        });
     }
 
-    this.problemTypeService.create(this.problemTypeForm.value).subscribe((problemType: any) => {
-      this.getAllProblemTypes(this.currentCompanyId);
-      this.closeModal();
-    });
-  }
+    ngOnInit() {
+        $('#modal-add-problem-type').on('hidden.bs.modal', () => {
+            this.closeModal();
+        });
+    }
 
-  closeModal() {
-    this.resetForm();
-    $('#modal-add-problem-type').modal('hide');
-  }
+    getAllProblemTypes(company_id): void {
+        this.problemTypeService.getAllProblemTypes(company_id).subscribe(
+            data => {
+                // this.problemTypes = data.results;
 
-  resetForm() {
-    this.problemTypeForm.reset({
-      company: config.api.base + 'company/' + this.currentCompanyId + '/',
-      problem_name: '',
-      tenant_view: false,
-      active: 'true',
-    });
-  }
+                const results: any[] = data.results;
+                let temp = results.sort((a: any, b: any) => {
+                    if (a.problem_name.toLowerCase() < b.problem_name.toLowerCase()) return -1;
+                    if (a.problem_name.toLowerCase() > b.problem_name.toLowerCase()) return 1;
+                    return 0;
+                });
+
+                var half_length = Math.ceil(temp.length / 2);
+                this.problemTypesList1 = temp.slice(0, half_length);
+                this.problemTypesList2 = temp.slice(half_length);
+            }
+        );
+    }
+
+    editProblemType(problemType) {
+        this.problemTypeForm.setValue(problemType);
+    }
+
+    onSubmit() {
+        // console.log(this.problemTypeForm.value);
+        if (this.problemTypeForm.value.id) {
+            this.problemTypeService.update(this.problemTypeForm.value).subscribe((problemType: any) => {
+                this.getAllProblemTypes(this.currentCompanyId);
+                this.closeModal();
+            });
+            return;
+        }
+
+        this.problemTypeService.create(this.problemTypeForm.value).subscribe((problemType: any) => {
+            this.getAllProblemTypes(this.currentCompanyId);
+            this.closeModal();
+        });
+    }
+
+    closeModal() {
+        this.resetForm();
+        $('#modal-add-problem-type').modal('hide');
+    }
+
+    resetForm() {
+        this.problemTypeForm.reset({
+            company: config.api.base + 'company/' + this.currentCompanyId + '/',
+            problem_name: '',
+            tenant_view: false,
+            active: 'true',
+        });
+    }
 }
 
