cc3b55739f344bd3794e81bb7408e6b565c784f8
mshahin364 <mshahin364@gmail.com>
2017-04-08 12:22:10 +0600

    SpinnerComponent added in loading-spinner module to resolve build failure

--- src/app/modules/shared/loading-spinner/loading-spinner.module.ts
+++ src/app/modules/shared/loading-spinner/loading-spinner.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from "@angular/common";
 import { LoadingSpinnerWaveComponent } from "app/modules/shared/loading-spinner/loading-spinner-wave.component";
 import { LoadingSpinnerPulseComponent } from "app/modules/shared/loading-spinner/loading-spinner-pulse.component";
 import { LoadingSpinnerRotatingPlaneComponent } from "app/modules/shared/loading-spinner/loading-spinner-rotating-plane.component";
+import { SpinnerComponent } from "app/modules/shared/loading-spinner/spinner.component";
 
 const LOADING_COMPONENT = [
+    SpinnerComponent,
     LoadingSpinnerWaveComponent,
     LoadingSpinnerPulseComponent,
     LoadingSpinnerRotatingPlaneComponent
