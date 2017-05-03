4c50be0db655bddd75471aa8d0220739813b214b
Tutul <tutul.net@gmail.com>
2017-05-03 12:42:58 +0600

    Refactored user profile save operation

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -27,16 +27,17 @@ export class DashboardComponent implements OnInit {
 
     userInfo: any;
     
-    userInfoForm = new FormGroup({
+    userProfileForm = new FormGroup({
+        rel_id: new FormControl(''),
+        url: new FormControl(''),
         first_name: new FormControl('', [Validators.required]),
         last_name: new FormControl('', [Validators.required]),
-        username: new FormControl('', [Validators.required]),
         email: new FormControl('', [Validators.required, ValidationService.emailValidator]),
         phone: new FormControl(''),
         fax: new FormControl(''),
         mobile: new FormControl(''),
-        new_password: new FormControl('', [Validators.required]),
-        confirm_password: new FormControl('', [Validators.required])
+        password: new FormControl(''),
+        confirm_password: new FormControl('')
     });
 
     employee: any = null;
@@ -55,76 +56,22 @@ export class DashboardComponent implements OnInit {
             ReadyDashboard.init();
         });
         this.userInfo = this.storage.getUserInfo();
-        let userId = this.userInfo.user_id;
-        // let storedUserInfo:any = null;
-        this.http.get('user/' + userId + '/', null, null).subscribe(user => {
-            this.userInfo.username = user.username;
-            let observable: Observable<any>;
-            if(this.userInfo.IsEmployee || this.userInfo.IsPropertyManager) {
-                this.http.get('employee/?user_id=' + userId).subscribe(data => {
-                    let employee = data.results.length > 0 ? data.results[0] : {'work_phone': '', 'mobile_phone': '', 'fax': ''};
-                    this.setContactInfo(employee.work_phone, employee.mobile_phone, employee.fax, employee.first_name, employee.last_name, employee.email);
-                    this.userInfoForm.reset(this.userInfo);
-                });
-            } else if(this.userInfo.IsContact || this.userInfo.IsVendor) {
-                let url = this.userInfo.IsVendor ? 'vendorcontact/?user_id=' + userId : 'tenantcontact/?user_id=' + userId
-                this.http.get(url).subscribe(data => {
-                    let contact = data.results.length > 0 ? data.results[0] : {'phone': '', 'mobile': '', 'fax': ''};
-                    this.setContactInfo(contact.phone, contact.mobile, contact.fax, contact.first_name, contact.last_name, contact.email);
-                    this.userInfoForm.reset(this.userInfo);
-                });
-            }
-            // else if(this.userInfo.IsVendor) {
-            //     this.http.get('vendorcontact/?user_id=' + userId).subscribe(data => {
-            //         let vendor = data.results.length > 0 ? data.results[0] : {'phone': '', 'mobile': '', 'fax': ''};
-            //         this.setContactInfo(vendor.phone, vendor.mobile, vendor.fax);
-            //     });
-            // }
-            // http://localhost:8080/api/employee/?user_id=1
-            // http://localhost:8080/api/tenantcontact/?user_id=10
-            // http://localhost:8080/api/vendorcontact/?user_id=7
 
-            // this.userInfoForm.reset(user);
-
+        // Get user profile information
+        this.http.get('userprofile/', null, null).subscribe(userProfile => {
+            this.userProfileForm.reset(userProfile);
         });
     }
 
-    setContactInfo(phone:string, mobile: string, fax:string, fname: string, lname:string, email:string) {
-        this.userInfo.phone = phone;
-        this.userInfo.mobile = mobile;
-        this.userInfo.fax = fax;
-        this.userInfo.first_name = fname;
-        this.userInfo.last_name = lname;
-        this.userInfo.email = email;
-    }
-
     onSubmit() {
+        // FIXME: Validation needs to be corrected
         // if (! this.userInfoForm.valid) return;
-        let data = this.userInfoForm.value;
-        let password = data.new_password; let phone = data.phone || ''; let mobile = data.mobile || ''; let fax = data.fax || '';
-        // let otherInfo = '+' + data.new_password + '+' + phone + '+' + mobile + '+' + fax + '+';
-        delete data.new_password;
+        let data = this.userProfileForm.value;
+        data.password = data.password || null;
         delete data.confirm_password;
-        delete data.phone;
-        delete data.mobile;
-        delete data.fax;
-
-        data.first_name = data.first_name + '+' + password + '+' + phone + '+';
-        data.last_name = data.last_name + '+' + mobile + '+' + fax + '+';
-        // let url = this.userInfo.url + 'update_userinfo/';
-        // let url = 'updateuser_info/'
-        // let url = this.userInfo.url;
-        let url = 'user/' + this.userInfo.user_id + '/';
-        this.http.put(url, data).subscribe(info => {
-            console.log('Userinfo patched successfully', info);
-            this.userInfo.first_name = info.first_name;
-            this.userInfo.last_name = info.last_name;
-            this.userInfo.email = info.email;
-            this.userInfo.phone = phone;
-            this.userInfo.mobile = mobile;
-            this.userInfo.fax = fax;
-            this.userInfoForm.reset(this.userInfo);
-            this.storage.set(config.storage.user, this.userInfo);
+        this.http.post(data.url, data).subscribe(userProfile => {
+            console.log('Userinfo patched successfully', userProfile);
+            this.userProfileForm.reset(userProfile);
             App.sidebar('close-sidebar-alt');
         });
     }
