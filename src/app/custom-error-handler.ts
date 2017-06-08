2465fbf292c1ee4db12335c13162aaf938bc9122
mshahin364 <mshahin364@gmail.com>
2017-06-08 22:56:02 +0600

    Only status 403 & 500<= status <=600 error handling

--- src/app/custom-error-handler.ts
+++ src/app/custom-error-handler.ts
@@ -6,11 +6,18 @@ declare var $: any;
 export class CustomErrorHandler implements ErrorHandler {
     constructor(private injector: Injector) { }
 
-    handleError (error: any): void {
+    handleError (response: any): void {
         const auth = this.injector.get(AuthenticationService);
 
         $('body').removeClass('modal-open');
         $('.modal-backdrop').remove();
-        auth.signout();
+
+        console.log("API ERROR:: ", response);
+
+        if (response.status == 403 || (response.status >= 500 && response.status < 600)) {
+            auth.signout();
+        }
+
+        // auth.signout();
     }
 }
